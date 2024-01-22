7926b180e5ff77c8a32213259ac06589230cd4f8
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-01-22 21:53:43 +0530

    bug-clear-code

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,40 +1,8 @@
-import * as React from 'react';
-import { styled, useTheme } from '@mui/material/styles';
-import SignIn from "../pages/SignIn/SignIn";
-import SignUp from "../pages/SignUp/SignUp";
-import DashBoard from '../pages/DashBoard/DashBoard';
-import { Route, Routes, Navigate } from 'react-router-dom';
-
-
 function App() {
-  const genKey = localStorage.getItem('hiruBro');
-  console.log(genKey);
-
   return (
     <>
-
-      {
-        genKey === null ? <>
-          <Routes>
-            <Route path={"*"} element={<Navigate to={'/login'} />} />
-            <Route path='/login' element={<SignIn />} />
-            <Route path='/signUp' element={<SignUp />} />
-          </Routes>
-
-        </> : <>
-        <DashBoard />
-        </>
-      }
-
-      {/* <Routes>
-        <Route path={"*"} element={<Navigate to={'/login'} />} />
-        <Route path='/login' element={<SignIn />} />
-        <Route path='/signUp' element={<SignUp />} />
-        <Route path='/dashboard' element={<DashBoard />} />
-
-      </Routes> */}
+  
     </>
   )
 }
-
 export default App
