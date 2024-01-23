050bab33c075fe740d8df645d9b02d3902f30989
imeshhirushan2117 <imeshhirushan2117@gmail.com>
2024-01-23 13:20:12 +0530

    table-deleted-edid-icon-added

--- src/Pages/StudentAction/StudentAction.jsx
+++ src/Pages/StudentAction/StudentAction.jsx
@@ -9,34 +9,45 @@ import { createTheme } from '@mui/material/styles';
 import { DeselectOutlined } from '@mui/icons-material';
 import { useState } from 'react';
 import instance from '../../services/Axious'
-import {AlertComponent} from '../../common/Aleart/Aleare';
+import { AlertComponent } from '../../common/Aleart/Aleare';
 
 export default function StudentAction() {
-  const [name,setName] = useState("")
-  const [age,setAge] = useState("")
-  const [address,setAddress] = useState("")
-  const [contact,setContact] = useState("")
+  const [name, setName] = useState("")
+  const [age, setAge] = useState("")
+  const [address, setAddress] = useState("")
+  const [contact, setContact] = useState("")
 
-  const save = () => { 
-    instance.post('/student/save',{
-      student_name:name,
-      student_age:age,
-      student_address:address,
-      student_contact:contact
+  const save = () => {
+    instance.post('/student/save', {
+      student_name: name,
+      student_age: age,
+      student_address: address,
+      student_contact: contact
     })
-    .then(function (response) {
-      console.log(response);
-      AlertComponent('success','Success...', 'Student Save Success!')
-      clearText()
-    })
-    .catch(function (error) {
-      console.log(error);
-      AlertComponent('error','Oops...', 'Something went wrong!')
-    });
+      .then(function (response) {
+        console.log(response);
+        AlertComponent('success', 'Success...', 'Student Save Success!')
+        clearText()
+      })
+      .catch(function (error) {
+        console.log(error);
+        AlertComponent('error', 'Oops...', 'Something went wrong!')
+      });
   }
 
   const update = () => {
-    console.log("update");
+    instance.put('/update/160', {
+      student_name: name,
+      student_age: age,
+      student_address: address,
+      student_contact: contact
+    })
+      .then((response) => {
+        console.log(response.data);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
   }
 
   const deleted = () => {
@@ -64,33 +75,33 @@ export default function StudentAction() {
         </Grid>
 
         <Grid item xs={6}>
-          <TextField value={age}  lable={'Age'} width={'100%'} onChange={(val) => setAge(val.target.value)}/>
+          <TextField value={age} lable={'Age'} width={'100%'} onChange={(val) => setAge(val.target.value)} />
         </Grid>
 
         <Grid item xs={6}>
-          <TextField value={address}  lable={'Address'} width={'100%'} onChange={(val) => setAddress(val.target.value)}/>
+          <TextField value={address} lable={'Address'} width={'100%'} onChange={(val) => setAddress(val.target.value)} />
         </Grid>
 
         <Grid item xs={6}>
-          <TextField value={contact}  lable={'Contact'} width={'100%'} onChange={(val) => setContact(val.target.value)}/>
+          <TextField value={contact} lable={'Contact'} width={'100%'} onChange={(val) => setContact(val.target.value)} />
         </Grid>
 
 
 
-       <Grid sx={{marginTop:'20px'}} item xs={6}>
-          <Button  name={'Save'} width={'100%'} color={'success'} onClick={() => save()}/>
+        <Grid sx={{ marginTop: '20px' }} item xs={6}>
+          <Button name={'Save'} width={'100%'} color={'success'} onClick={() => save()} />
         </Grid>
 
-        <Grid sx={{marginTop:'20px'}} item xs={6}>
-          <Button name={'Update'} width={'100%'} color={'warning'} onClick={() => update()}/>
+        <Grid sx={{ marginTop: '20px' }} item xs={6}>
+          <Button name={'Update'} width={'100%'} color={'warning'} onClick={() => update()} />
         </Grid>
 
-        <Grid sx={{marginTop:'20px'}} item xs={6}>
-          <Button name={'Deleted'} width={'100%'} color={'error'} onClick={() => deleted()}/>
+        <Grid sx={{ marginTop: '20px' }} item xs={6}>
+          <Button name={'Deleted'} width={'100%'} color={'error'} onClick={() => deleted()} />
         </Grid>
 
-        <Grid sx={{marginTop:'20px'}} item xs={6}>
-          <Button name={'Clear'} width={'100%'} color={'info'} onClick={() => clear()}/>
+        <Grid sx={{ marginTop: '20px' }} item xs={6}>
+          <Button name={'Clear'} width={'100%'} color={'info'} onClick={() => clear()} />
         </Grid>
 
 
--- src/Pages/ViewStudent/ViewStudent.jsx
+++ src/Pages/ViewStudent/ViewStudent.jsx
@@ -2,6 +2,23 @@ import * as React from 'react';
 import { DataGrid } from '@mui/x-data-grid';
 import instance from '../../services/Axious'
 import { useState, useEffect } from 'react';
+import IconButton from '@mui/material/IconButton';
+import DeleteIcon from '@mui/icons-material/Delete';
+import EditIcon from '@mui/icons-material/Edit';
+
+
+
+const handleDelete =() => {
+  console.log("deleted");
+}
+
+const handleEdit =() => {
+  console.log("edit");
+}
+
+
+
+
 
 const columns = [
   { field: 'id', headerName: 'ID', width: 70 },
@@ -9,13 +26,31 @@ const columns = [
   { field: 'age', headerName: 'Age', type: 'number', width: 90, },
   { field: 'address', headerName: 'Address', width: 100 },
   { field: 'contact', headerName: 'Contact', width: 100 },
-];
+  {
+    field: 'actions',
+    headerName: 'Actions',
+    width: 150,
+    renderCell: (params) => (
+      <div>
+        <IconButton
+        color='info'
+          aria-label="edit"
+          onClick={() => handleEdit(params.row.id)}
+        >
+          <EditIcon />
+        </IconButton>
+        <IconButton
+        color='error'
+          aria-label="delete"
+          onClick={() => handleDelete(params.row.id)}
+        >
+          <DeleteIcon />
+        </IconButton>
+      </div>
+    ),
+  },
 
-const rows = [
-  // { id: 1, name: 'Imesh', age: 24, address: 'Panadura', contact: '0779201232' },
-  // { id: 2, name: 'Hirushan', age: 18, address: 'Horana', contact: '0727681232' }
 ];
-
 
 export default function ViewStudent() {
 
@@ -28,19 +63,20 @@ export default function ViewStudent() {
     })
       .then(function (response) {
         const array = response.data.map(val => ({
-           id: val.id, 
-           name: val.student_name,
-            age: val.student_age, 
-            address:val.student_address, 
-            contact: val.student_contact 
+          id: val.id,
+          name: val.student_name,
+          age: val.student_age,
+          address: val.student_address,
+          contact: val.student_contact,
         }));
-
         setData(array);
       });
   }, []);
 
+
   return (
     <div style={{ height: 400, width: '100%' }}>
+
       <DataGrid
         rows={data}
         columns={columns}
@@ -52,6 +88,9 @@ export default function ViewStudent() {
         pageSizeOptions={[5, 10]}
         checkboxSelection
       />
+
+
+
     </div>
   )
 }
